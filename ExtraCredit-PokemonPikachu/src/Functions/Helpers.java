a7e9c05b00e5f523f1bbe635ed4eb1aadbe77246
Diego Mendez <d.mendez@correo.unimet.edu.ve>
2024-04-03 14:38:03 -0400

    Creacion del save preliminar

--- ExtraCredit-PokemonPikachu/src/Functions/Helpers.java
+++ ExtraCredit-PokemonPikachu/src/Functions/Helpers.java
@@ -5,6 +5,8 @@
 package Functions;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  *
@@ -14,5 +16,11 @@ public class Helpers {
     public void saveExist() {
     File archivo = new File(System.getProperty("user.dir") + "\\src\\PokeFile\\pokesave.txt");
         System.out.println(archivo.exists());
+    }
+    public void crearSave() throws IOException {
+        
+        FileWriter pokeFichero = new FileWriter (System.getProperty("user.dir") + "\\src\\PokeFile\\pokesave.txt");
+        pokeFichero.write("pepino");
+        pokeFichero.close();
     }
 }
