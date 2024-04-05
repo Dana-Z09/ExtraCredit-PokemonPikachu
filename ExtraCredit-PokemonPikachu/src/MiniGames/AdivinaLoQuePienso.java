c36ddf8a35af48ca168bb0edf0ee7cfe13095669
Diego Mendez <d.mendez@correo.unimet.edu.ve>
2024-04-05 00:45:53 -0400

    wefw

--- ExtraCredit-PokemonPikachu/src/MiniGames/AdivinaLoQuePienso.java
+++ ExtraCredit-PokemonPikachu/src/MiniGames/AdivinaLoQuePienso.java
@@ -24,6 +24,36 @@ public class AdivinaLoQuePienso {
             System.out.println(objetoActual.getCost());
             
         }
-      return wattsApostados;
+        //ArrayUsado para contener los objetos posibles
+        String[] rango = {"Baya Aranja","Baya Caquic", "Pokeball","Baya Zreza","Baya Zanama","Baya Ziuela","Baya Zidra", "Baya Enigma", "Caramelo raro"};
+        int answer = JOptionPane.showOptionDialog(null, "   En que objeto piensa el Pokemon?  " ,"Juego Adivina lo que pienso" , JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, null, rango, rango[0]);
+        //La respuesta depende de que objeto se haya elegido en el Optiondialog, usando la lenght del array rango
+        System.out.println(answer);
+        //Este array es de probabilidades, mientras mas aparezca un numero ahi mas probable sera que coincida con el escogido en el Joptiondialog
+        ArrayList objetosRecibidosPosibles = new ArrayList();
+        //Recorre la ArrayList de hsitorial
+        for (int i = 0; i < historial.size(); i++) {
+            Gift objetoActual = (Gift) historial.get(i);
+            //El numero sale dependiendo de la quantity de ese objeto en esa posicion del primer ArrayList
+            for (int j = 0; j < objetoActual.getQuantity(); j++) {   
+                objetosRecibidosPosibles.add(i);
+            }
+        }
+        Random randomNumber1 = new Random();
+        //Se crea un numero aleatorio entre 0 y la length del ArrayList objetosrecibidosPosible
+        int numRandom = randomNumber1.nextInt(objetosRecibidosPosibles.size());
+        //Luego se toma el numero qeu este en la posicion que haya dicho numRandom
+        int numObjeto = (int) objetosRecibidosPosibles.get(numRandom);
+        //Si el numObjeto coincide con la posicion del ArrayList historial que elegio el jugador entonces se ganan los watts
+        if (answer == numObjeto) {
+            //Prueba en terminal
+            System.out.println("Ganaste, coincidio " + answer +"con " + numObjeto);
+            JOptionPane.showMessageDialog(null, "yippiee");
+            return wattsApostados;
+        } else {
+            JOptionPane.showMessageDialog(null, "Perdiste");
+            return -wattsApostados;
+        }
+     
     }
 }
